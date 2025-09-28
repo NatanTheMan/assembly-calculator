d30916dd3036dc49af80db3a3375c215d9039fc6
Natan <natanscosta7@gmail.com>
2025-09-28 19:12:59 -0300

    func to read simple input

--- main.asm
+++ main.asm
@@ -1,9 +1,21 @@
+section .bss
+  buf: resb 10
+
 section .text
   global _start
 
 _start:
+  call read_input
   
   jmp exit
+
+read_input:
+  mov rax, 0
+  mov rdi, 0
+  mov rsi, buf
+  mov rdx, 10
+  syscall
+  ret
 
 exit:
   mov rax, 60
