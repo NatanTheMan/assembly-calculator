77b21df73067d4c49352518307398e483ccd4903
Natan <natanscosta7@gmail.com>
2025-09-28 19:28:27 -0300

    read second number

--- main.asm
+++ main.asm
@@ -1,14 +1,17 @@
 section .bss
-  buf: resb 10
+  first_num: resb 10
+  second_num: resb 10
 
 section .data
   msg_first_input: db "Enter first number: "
+  msg_second_input: db "Enter second number: "
 
 section .text
   global _start
 
 _start:
   call read_first_number
+  call read_second_number
   
   jmp exit
 
@@ -19,6 +22,18 @@ read_first_number:
   mov rdx, 20
   syscall
 
+  mov rsi, first_num
+  call read_input
+  ret
+
+read_second_number:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, msg_second_input
+  mov rdx, 21
+  syscall
+
+  mov rsi, second_num
   call read_input
   ret
 
@@ -26,7 +41,6 @@ read_first_number:
 read_input:
   mov rax, 0
   mov rdi, 0
-  mov rsi, buf
   mov rdx, 10
   syscall
   ret
