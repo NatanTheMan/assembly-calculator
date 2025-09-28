a6fd85aba64cf888fd9cacb2f3856afbe112317c
Natan <natanscosta7@gmail.com>
2025-09-28 19:23:54 -0300

    read first number

--- main.asm
+++ main.asm
@@ -1,13 +1,27 @@
 section .bss
   buf: resb 10
 
+section .data
+  msg_first_input: db "Enter first number: "
+
 section .text
   global _start
 
 _start:
-  call read_input
+  call read_first_number
   
   jmp exit
+
+read_first_number:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, msg_first_input
+  mov rdx, 20
+  syscall
+
+  call read_input
+  ret
+
 
 read_input:
   mov rax, 0
